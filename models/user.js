0ddc91f5300041693db556d76a5e111fe2788146
kishansuvagiya <suvagiya.kishan96@gmail.com>
2023-12-30 18:31:11 +0530

    email schema in user model modified

--- models/user.js
+++ models/user.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  fullname: String,
-  email: {type:String, unique: true},
+  fullname: {
+    type:String,
+    lowercase: true
+  },
+  email: { 
+    type: String, 
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
   password: String
 });
 
